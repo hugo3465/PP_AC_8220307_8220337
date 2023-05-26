2e2e82b34ee7de991da2c25ca452250fdc33f3c3
hugo3465 <100604532+hugo3465@users.noreply.github.com>
2023-05-27 00:04:17 +0100

    Initial commit

---
+++ src/pp_ac_8220307_8220337/PP_AC_8220307_8220337.java
@@ -0,0 +1,21 @@
+package pp_ac_8220307_8220337;
+
+/**
+ * Nome: Pedro Marcelo Santos Pinho
+ * Número: 8220307
+ * Turma: LEIT2
+ * 
+ * Nome: Hugo Ricardo Almeida Guimarães
+ * Número: 8220337
+ * Turma: LEIT2
+ */
+public class PP_AC_8220307_8220337 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

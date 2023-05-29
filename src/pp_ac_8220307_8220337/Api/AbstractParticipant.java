1359c95a16d42381e70193de80f0642d9dfc402c
hugo3465 <huguito.ricardo@hotmail.com>
2023-05-29 21:43:41 +0100

    Inserção de packages e da classe AbstractParticipant
    Eu acho que já tinha colocado os packages no commit anterior, mas eles não passaram para o github não sei porquê

---
+++ src/pp_ac_8220307_8220337/Api/AbstractParticipant.java
@@ -0,0 +1,59 @@
+/**
+ * Nome: Pedro Marcelo Santos Pinho
+ * Número: 8220307
+ * Turma: LEIT2
+ * 
+ * Nome: Hugo Ricardo Almeida Guimarães
+ * Número: 8220337
+ * Turma: LEIT2
+ */
+
+package pp_ac_8220307_8220337.Api;
+
+import cbl.participants.Contact;
+import cbl.participants.Instituition;
+import cbl.participants.Participant;
+
+/**
+ *
+ * @author User
+ */
+public class AbstractParticipant implements Participant {
+    
+    private int idParicipant;
+    private String name;
+    private String email;
+    private Contacto contact;
+    private Institution institution;
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getEmail() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Contact getContact() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Instituition getInstituition() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setInstituition(Instituition instn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setContact(Contact cntct) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
--- src/pp_ac_8220307_8220337/PP_AC_8220307_8220337.java
+++ src/pp_ac_8220307_8220337/PP_AC_8220307_8220337.java
@@ -1,5 +1,3 @@
-package pp_ac_8220307_8220337;
-
 /**
  * Nome: Pedro Marcelo Santos Pinho
  * Número: 8220307
@@ -9,13 +7,16 @@ package pp_ac_8220307_8220337;
  * Número: 8220337
  * Turma: LEIT2
  */
+
+package pp_ac_8220307_8220337;
+
 public class PP_AC_8220307_8220337 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hellow World");
     }
     
 }

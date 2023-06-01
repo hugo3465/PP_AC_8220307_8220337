c51c4212336e224b8439252f6f73a50c0154c99d
Pedro Pinho <116769327+PedroPinho17@users.noreply.github.com>
2023-06-01 22:09:31 +0100

    Update menu

--- src/pp_ac_8220307_8220337/Application/MenuAdminManagement.java
+++ src/pp_ac_8220307_8220337/Application/MenuAdminManagement.java
@@ -8,6 +8,34 @@ package pp_ac_8220307_8220337.Application;
  *
  * @author pedro
  */
-public class MenuAdminManagement {
-    
+public class MenuAdminManagement implements MenuDisplay {
+
+    @Override
+    public void display() {
+        System.out.println("Bem-Vindo ao Menu do Administrador ");
+        System.out.println("Escolha a opção que deseja");
+        System.out.println("1 - Editar");
+        System.out.println("10 - Sair");
+    }
+
+    public static void hadleAdminmenu(MenuManager menuManager) {
+        MenuAdminManagement menuAdminManagement = new MenuAdminManagement();
+        Boolean isAdminMenuRunning = true;
+
+        while (isAdminMenuRunning) {
+            int option = menuManager.getInput();
+
+            switch (option) {
+                case 1:
+                    break;
+                case 10:
+                    isAdminMenuRunning = false;
+                    return;
+                default:
+                    System.out.println("Invalid option. Please Try again");
+                    break;
+            }
+        }
+    }
+
 }
